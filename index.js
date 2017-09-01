6c68d81199aadffbd57ecb43768ee2dadaf9b99a
Iain Donaldson <iain@sotp.xyz>
2017-09-01 09:03:37 +0100

    Simplified to replacethe env variable only so it can run in browser with no issue

--- index.js
+++ index.js
@@ -17,18 +17,9 @@ module.exports = function (options) {
           if (t.isStringLiteral(key)) {
             var name = key.value;
             var value = state.opts.env && name in state.opts.env ? state.opts.env[name] : process.env[name];
-            var me = t.memberExpression;
-            var i = t.identifier;
-            var le = t.logicalExpression;
 
             path.replaceWith(
-              le('||', 
-                le('&&', 
-                  le('&&', i('process'), me(i('process'), i('env'))),
-                  me(i('process.env'), i(name))
-                ), 
                 t.valueToNode(value)
-              )
             );
           }
         }
